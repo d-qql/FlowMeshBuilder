968f993d043a5a0ba032315b9510be662da2676e
tungstencucumber <44pyriaiev44@phystech.edu>
2021-05-16 20:36:30 +0300

    Рефакторинг интерполятора под новый формат меша

--- Interpolator/Interpolator.cpp
+++ Interpolator/Interpolator.cpp
@@ -14,21 +14,21 @@ std::array<double, 3> Interpolator(const Node& node, idx_t cellIndex, const Mesh
     std::array<double, 3> InterpolatedField = {0., 0., 0.};
     std::vector<idx_t> Neighbours;
     Neighbours.push_back(cellIndex);
-    Neighbours.insert(Neighbours.begin(), mesh.InternalCells.at(cellIndex).neighbours.begin(), mesh.InternalCells.at(cellIndex).neighbours.end());
+    Neighbours.insert(Neighbours.begin(), mesh.Cells.at(cellIndex).neighbours.begin(), mesh.Cells.at(cellIndex).neighbours.end());
 
     double DistanceSum = 0;
     unsigned int N = Neighbours.size();
     std::vector<double> Distances;
     Distances.resize(N);
-    for (std::vector<idx_t>::iterator it = Neighbours.begin(); it != Neighbours.end(); ++it) {
-        double CurrentDistance = sqrt(GetDistanceSquared(node, mesh.InternalCells.at(*it).center_pos));
+    for (unsigned int i = 0; i < N; ++i) {
+        double CurrentDistance = sqrt(GetDistanceSquared(node, mesh.Cells[Neighbours[i]].center_pos));
         Distances.push_back(CurrentDistance);
         DistanceSum += CurrentDistance;
     }
     for (unsigned int j = 0; j < Neighbours.size(); ++j) {
         double CurrentWeight = (2/N - Distances[j] / DistanceSum);
         for (unsigned int i = 0; i < 3; ++i)
-            InterpolatedField[i] += mesh.InternalCells.at(Neighbours[j]).vector_val[i] * CurrentWeight;
+            InterpolatedField[i] += mesh.Cells[Neighbours[j]].vector_val[i] * CurrentWeight;
     }
     return InterpolatedField;
 }
