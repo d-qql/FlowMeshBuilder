d6c07fdbeb838e8478d6ac70571d7f079ebca598
d-qql <petrov.dmitrii@phystech.edu>
2021-05-17 14:13:57 +0300

    Typos fix

--- Linal/utility.h
+++ Linal/utility.h
@@ -8,7 +8,7 @@
 
 double TripleProduct3(const std::array<double, 3>& v0, const std::array<double, 3>& v1, const std::array<double, 3>& v2);
 
-std::array<double, 3> VectorProduct(const std::array<double, 3>& v0, const std::array<double, 3>& v1);
+std::array<double, 3> CrossProduct(const std::array<double, 3>& v0, const std::array<double, 3>& v1);
 
 std::array<double, 3> operator*=(std::array<double, 3> arr, double k);
 std::array<double, 3> operator*(std::array<double, 3> arr, double k);
